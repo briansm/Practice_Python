2c5b93c0cead3e39f40d809945a6d5c3853f0530
Brian Morales <briansmor09@gmail.com>
2018-05-20 12:17:23 -0600

    Menu

---
+++ Practice_Python.py
@@ -0,0 +1,11 @@
+#-*- Coding: utf-8 -*-
+
+
+
+
+
+if __name__=='__main__':
+    option=int(raw_input("Seleccione una opcion: \n "
+    +"1.Dibujar un triangulo \n "
+    +"2.Palabra palindroma \n "
+    +"3.Tabla de Multiplicar \n "))
